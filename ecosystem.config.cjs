3b6689e55d38d66bb029b66caebef7dce0484c12
WASPEI2 <aekapab10@gmail.com>
2024-02-15 15:48:44 +0700

    add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: "Zillionassets",
+      script: "serve",
+      watch: true,
+      env: {
+        PM2_SERVE_PATH: "./dist",
+        PM2_SERVE_PORT: 5173,
+        PM2_SERVE_SPA: "true",
+        NODE_ENV: "production",
+        NODE_ENV: "development",
+        PORT: 5137,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "127.0.0.1",
+      ref: "origin/main",
+      repo: "git@github.com:WASPEl2/zillionassets.git",
+      path: "./",
+      "pre-deploy-local": 'echo "Running pre-deploy tasks..."',
+      "post-deploy":
+        "yarn install && yarn build && pm2 startOrRestart ecosystem.config.cjs --env production",
+    },
+  },
+};
--- src/data.jsx
+++ src/data.jsx
@@ -2,5 +2,5 @@ export const bannerData = [
     {900: 'Premium Product'}, 
 ]
 export const config = {
-    api: 'https://159.138.231.127:5000', 
+    api: 'http://127.0.0.1:5000', 
 };
